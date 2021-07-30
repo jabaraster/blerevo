69faddf3e8564130d86c3b7179eb9542d3cc08c7
jabaraster <ah@jabara.info>
2021-07-30 11:49:03 +0900

    とりあえず保存. 全然上手く動かない.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,24 +1,10 @@
+import fb from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 import * as functions from 'firebase-functions';
-import * as firebase from "firebase";
-import * as admin from "firebase-admin";
-import Axios from "axios";
+import axios from 'axios';
 
-const COLLECTION_ID = "field-boss-cycle-2";
-const RE_NOTIFICATION_THRESHOLD = 10;
-const WITHIN_MINUTE_NORMAL_BOSS = 3;
-const WITHIN_MINUTE_REPLACE_BOSS = 6;
-
-// firebase.initializeApp({
-//     projectId: "blade-and-soul-field-bos-c21bf",
-// });
-
-const serviceAccount = require("./serviceAccount.json")
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-});
-
-firebase.initializeApp({
+export const firebase = !fb.apps.length ? fb.initializeApp({
     apiKey: "AIzaSyA8OgTiooOW4F97YTBVw5PuaR1p9oo4R9g",
     authDomain: "blade-and-soul-field-bos-c21bf.firebaseapp.com",
     databaseURL: "https://blade-and-soul-field-bos-c21bf.firebaseio.com",
@@ -27,21 +13,14 @@ firebase.initializeApp({
     messagingSenderId: "176293133121",
     appId: "1:176293133121:web:570cb3854312fea1ab7fc0",
     measurementId: "G-2YYBYEEG7G"
-});
+}) : fb.app()
 
+const firestore = firebase.firestore()
 
-const firestore = firebase.firestore();
-
-export async function sendMessageSample() {
-    console.log('sendMessageSample')
-    await admin.messaging().send({
-        notification: {
-            title: "テスト",
-            body: "テストです。",
-        },
-        token: "cp5h4wvnlkLW4kY39kAdKn:APA91bHWCfFMI5eiN3lo6yv0y2dFZSR149RBYEvlKRvJzh16xgEurFrDlA82XQGLgLeUOJ-Q4vF6QGLi1s7cnsbDLnVnuX1kGx0j6t4xt5g9yuOXdxLQnxFg0492apUp8I7UDj2dctRl"
-    })
-}
+const COLLECTION_ID = "field-boss-cycle-2";
+const RE_NOTIFICATION_THRESHOLD = 10;
+const WITHIN_MINUTE_NORMAL_BOSS = 3;
+const WITHIN_MINUTE_REPLACE_BOSS = 6;
 
 function env(name: string): string {
     const notf = functions.config().notification;
@@ -53,7 +32,12 @@ function env(name: string): string {
     }
 }
 
-export const notification = functions.region("asia-northeast1").pubsub.schedule("every 1 minutes").onRun(async context => {
+// export const personalizedNotification = functions.region("asia-northeast1")
+//     .https.onCall((data: any, _: any) => {
+//     return data
+// })
+
+exports.notification = functions.region("asia-northeast1").pubsub.schedule("every 1 minutes").onRun(async context => {
     await notificationCore("ケヤキ");
     await notificationCore("サクラ");
 });
@@ -76,9 +60,6 @@ export const notificationCore = async function(server: string) {
             if (!withinMinute(boss.region === "入れ替わるFB" ? WITHIN_MINUTE_REPLACE_BOSS : WITHIN_MINUTE_NORMAL_BOSS, now, boss.nextPopTime)) {
                 return false;
             }
-            // if (boss.force) { // 勢力ボスは追わない…
-            //     return false;
-            // }
             if (boss.repopIntervalMinutes <= 60) { // 頻繁にリポップスするボスは鬱陶しいので割愛
                 return false;
             }
@@ -89,14 +70,9 @@ export const notificationCore = async function(server: string) {
             if (!notificated) {
                 return true;
             }
-            console.log("----------------");
-            console.log(notificated.data());
-            console.log(notificated.data().notificatedTime.toDate());
-            console.log(withinMinute(RE_NOTIFICATION_THRESHOLD, notificated.data().notificatedTime.toDate(), now));
             return !withinMinute(RE_NOTIFICATION_THRESHOLD, notificated.data().notificatedTime.toDate(), now);
         });
 
-    console.log(bossList);
     if (bossList.length === 0) {
         return;
     }
@@ -119,7 +95,7 @@ export const notificationCore = async function(server: string) {
             await notificated.ref.update({ notificatedTime: now });
         }
     }));
-    const notificationPromise = Axios.post(`https://api.push7.jp/api/v1/${env("push_7_appno")}/send`, {
+    const notificationPromise = axios.post(`https://api.push7.jp/api/v1/${env("push_7_appno")}/send`, {
         apikey: env("push_7_apikey"),
         title: "HASTOOLフィルボ通知",
         body: text,
@@ -142,13 +118,17 @@ function buildNotificationMessage(now: Date, boss: ExFieldBossCycle): string {
     return `${remainMinute}分後 ${boss.name}`;
 }
 
+type Timestamp = fb.firestore.Timestamp;
+type Notificated = fb.firestore.QueryDocumentSnapshot<fb.firestore.DocumentData>;
+type DocumentData = fb.firestore.DocumentData;
+type CollectionReference<T> = fb.firestore.CollectionReference<T>;
 interface FieldBossCycle {
     name: string;
     id: string;
     region: string;
     area: string;
     force: boolean,
-    lastDefeatedTime: firebase.firestore.Timestamp;
+    lastDefeatedTime: Timestamp;
     repopIntervalMinutes: number;
     sortOrder: number;
     reliability : boolean;
@@ -156,8 +136,6 @@ interface FieldBossCycle {
 interface ExFieldBossCycle extends FieldBossCycle {
     nextPopTime: Date;
 }
-
-type Notificated = firebase.firestore.QueryDocumentSnapshot<firebase.firestore.DocumentData>;
 
 function nextPopTime(now: Date, boss: FieldBossCycle): Date {
     let t = null;
@@ -168,11 +146,11 @@ function nextPopTime(now: Date, boss: FieldBossCycle): Date {
     return t;
 }
 
-function getBossCycleListCollection(server: string): firebase.firestore.CollectionReference<firebase.firestore.DocumentData> {
+function getBossCycleListCollection(server: string): CollectionReference<DocumentData> {
     return firestore.collection(`${COLLECTION_ID}/${server}/cycles/`);
 }
 
-function getNotificatedCollection(server: string): firebase.firestore.CollectionReference<firebase.firestore.DocumentData> {
+function getNotificatedCollection(server: string): CollectionReference<DocumentData> {
     return firestore.collection(`${COLLECTION_ID}/${server}/notificated/`);
 }
 
@@ -181,7 +159,7 @@ async function listCycles(server: string): Promise<FieldBossCycle[]> {
     await getBossCycleListCollection(server)
     .orderBy("sortOrder", "asc")
     .get()
-    .then(d => {
+    .then((d) => {
         d.forEach(r => {
             ret.push(r.data());
         });
